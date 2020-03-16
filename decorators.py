636d1687cd33ff37ad58575bffeb82d961e44a2f
winters <ajwinters@mail.com>
2020-03-16 16:39:32 +0000

    Changed output formatting to make tests more easily read.

--- decorators.py
+++ decorators.py
@@ -2,21 +2,23 @@
 # A small project to assure myself that I have grasped how Python decorators work
 #################################################################################
 
+import sys
+
 # Function-based decorator:
 #
 # x = decorator_function(x) : x is a function or class
 
 
 def my_decorator_function (x):
-    def wrapper ():
-    	print("Function-decorated {}.".format(x))
+    def decorated ():
+    	print("{} decorated by function.".format(x))
     	return x()
-    return wrapper
+    return decorated
 
 
 @my_decorator_function
 def my_decorated_function ():
-	print("1 Called.")
+    print("{} Called.".format(sys._getframe(  ).f_code.co_name))
 
 
 @my_decorator_function
@@ -26,8 +28,10 @@ class My_Decorated_Class (object):
 
 
 # Test:
-print("\n# Function-based decorator test:\n")
+print("\n# Function-based decorator test:")
+print("################################\n")
 my_decorated_function()
+print("\n")
 My_Decorated_Class()
 
 
@@ -39,19 +43,19 @@ My_Decorated_Class()
 
 def my_decorator_function_wa (*args, **kwargs):
     def inner_decorator (x):
-        def wrapper ():
-            print("Function-decorated {}.".format(x))
+        def decorated ():
+            print("{} decorated by function.".format(x))
             print("Arguments passed:")
             print(args)
             print(kwargs)
             return x()
-        return wrapper
+        return decorated
     return inner_decorator
 
 
 @my_decorator_function_wa(1, 2, 3, some_boolean=True, some_string="abc")  
 def my_decorated_function2 ():
-    print("2 Called.")
+    print("{} Called.".format(sys._getframe(  ).f_code.co_name))
 
 
 @my_decorator_function_wa(1, 2, 3, some_boolean=True, some_string="abc")  
@@ -61,8 +65,10 @@ class My_Decorated_Class2 (object):
 
 
 # Test:
-print("\n# Function-based decorator test with arguments:\n")
+print("\n# Function-based decorator test with arguments:")
+print("###############################################\n")
 my_decorated_function2()
+print("\n")
 My_Decorated_Class2()
 
 
@@ -76,13 +82,13 @@ class My_Decorator_Class (object):
         self.x = x
 
     def __call__ (self):
-        print("Class-decorated {}.".format(self.x))
+        print("{} decorated by class.".format(self.x))
         return self.x()
 
 
 @My_Decorator_Class
 def my_decorated_function3 ():
-    print("3 Called.")
+    print("{} Called.".format(sys._getframe(  ).f_code.co_name))
 
 
 @My_Decorator_Class
@@ -92,8 +98,10 @@ class My_Decorated_Class3 (object):
 
 
 # Test    
-print("\n# Class-based decorator test:\n")
+print("\n# Class-based decorator test:")
+print("#############################\n")
 my_decorated_function3()
+print("\n")
 My_Decorated_Class3()
 
 
@@ -109,18 +117,18 @@ class My_Decorator_Class_WA (object):
         self.kwargs = kwargs
 
     def __call__ (self, x):
-        def wrapper ():
-            print("Class-decorated {}.".format(x))
+        def decorated ():
+            print("{} decorated by class.".format(x))
             print("Arguments passed:")
             print(self.args)
             print(self.kwargs)
             return x()
-        return wrapper
+        return decorated
 
 
 @My_Decorator_Class_WA(1, 2, 3, some_boolean=True, some_string="abc")
 def my_decorated_function4 ():
-    print("4 Called.")
+    print("{} Called.".format(sys._getframe(  ).f_code.co_name))
 
 
 @My_Decorator_Class_WA(1, 2, 3, some_boolean=True, some_string="abc")
@@ -130,6 +138,8 @@ class My_Decorated_Class4 (object):
 
 
 # Test    
-print("\n# Class-based decorator test with arguments:\n")
+print("\n# Class-based decorator test with arguments:")
+print("############################################\n")
 my_decorated_function4()
+print("\n")
 My_Decorated_Class4()        
